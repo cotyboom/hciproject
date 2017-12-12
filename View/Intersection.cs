1ee1fbb3577d0e9ab839780c0f205b8514e19f6a
peetha <33936121+peetha@users.noreply.github.com>
2017-12-13 00:14:55 +0200

    Add files via upload

---
+++ View/Intersection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Alice_in_SpringLand.View
+{
+    public partial class Intersection : Form
+    {
+        public Intersection()
+        {
+            InitializeComponent();
+        }
+
+        private void panelFlowers_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panelFruits_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.buton_fructe);
+            player.Play();
+        }
+
+        private void panelFlowers_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.buton_flori);
+            player.Play();
+        }
+
+        private void panelTraditions_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.buton_traditii);
+            player.Play();
+        }
+
+        private void panelActivities_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.buton_activitati);
+            player.Play();
+        }
+    }
+}
